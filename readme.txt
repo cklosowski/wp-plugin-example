55083c11767b96b13f419174fce3e1ed34148e00
Chris Klosowski <cklosowski@gmail.com>
2012-11-11 11:59:50 -0700

    Initial Setup of the plugin
    Adding in the readme.txt and the main plugin file with admin pages and a
    single option.

---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== WordPress Plugin Example === 
+Contributors: cklosows 
+Tags: example, desert code camp, plugin
+Stable tag: 1.0
+Requires at least: 3.0
+Tested up to: 3.5-beta2
+Donate link: http://www.chriskdesigns.com/
+ 
+This is the example plugin for WordPress Plugin Bootcamp @ Desert Code Camp 2012.
+
+== Installation ==
+1. Download the .zip file
+2. Unpack into wp-content/plugins/ folder
+3. Activate the plugin
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+–Version 1.0–
+This is the initial release of the plugin.
+
+== Description ==
+None at this time.
+
+== Upgrade Notice ==
+If there are any major issues with upgrading or possible gotcha's list them here.
+
+== Screenshots ==
+Stored in your assets directory of the repository
---
+++ wp-plugin-example.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: WordPress Plugin Example
+Plugin URI: http://www.chriskdesigns.com/
+Description: This is the example plugin for WordPress Plugin Bootcamp @ Desert Code Camp 2012.
+Version: 1.0
+Author: Chris Klosowski
+Author URI: http://www.chriskdesigns.com/
+License: GPLv2 or later
+*/
+
+class CKWPPExamplePlugin {
+	private static $ckwppe_instance;
+
+	private function __construct() {
+		$this->constants(); // Defines any constants used in the plugin
+		$this->init();   // Sets up all the actions and filters
+	}
+
+	public static function getInstance() {
+		if ( !self::$ckwppe_instance ) {
+			self::$ckwppe_instance = new CKWPPExamplePlugin();
+		}
+
+		return self::$ckwppe_instance;
+	}
+
+	private function constants() {
+		define( 'CKWPPE_VERSION', '1.0' );
+		define( 'CKWPPE_TEXT_DOMAIN', 'ckwppe' );
+	}
+
+	private function init() {
+		// Register the options with the settings API
+		add_action( 'admin_init', array( $this, 'ckwppe_register_settings' ) );
+
+		// Add the menu page
+		add_action( 'admin_menu', array( $this, 'ckwppe_setup_admin' ) );
+	}
+
+	public function ckwppe_register_settings() {
+		register_setting( 'ckwppe-update-options', 'ckwppe_enable' );
+	}
+
+	public function ckwppe_setup_admin() {
+		// Add our Menu Area
+		add_options_page( __( 'Example Plugin', CKWPPE_TEXT_DOMAIN ), __( 'Example', CKWPPE_TEXT_DOMAIN ), 'administrator', 'ckwppe-settings', array( $this, 'ckwppe_admin_page' ) );
+	}
+
+	public function ckwppe_admin_page() {
+		?>
+		<div class="wrap">
+			<div id="icon-options-general" class="icon32"></div><h2>WordPress Example Plugin</h2>
+			<form method="post" action="options.php">
+				<?php wp_nonce_field( 'ckwppe-update-options' ); ?>
+				<table class="form-table">
+					<tr valign="top">
+						<th scope="row"><?php _e( 'Options:', CKWPPE_TEXT_DOMAIN ); ?><br /><span style="font-size: x-small;"><?php _e( 'With great power, comes great responsiblity.', CKWPPE_TEXT_DOMAIN ); ?></span></th>
+						<td>
+							<input type="checkbox" name="ckwppe_enable" value="1" <?php if ( get_option( 'ckwppe_enable' ) ) {?>checked="checked"<?php ;}?> /> <?php _e( 'Enable the Example Plugin to output', CKWPPE_TEXT_DOMAIN ); ?>
+						</td>
+					</tr>
+					<input type="hidden" name="action" value="update" />
+					<input type="hidden" name="page_options" value="ckwppe_primary_setting" />
+
+					<?php settings_fields( 'ckwppe-update-options' ); ?>
+				</table>
+				<input type="submit" class="button-primary" value="<?php _e( 'Save Changes' ) ?>" />
+			</form>
+		</div>
+		<?php
+	}
+}
+
+
+$GLOBALS['ckwppe'] = CKWPPExamplePlugin::getInstance();
